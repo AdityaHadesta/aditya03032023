1f6cdbcf9ae5a11f80824c2901c42e8cd0ae3045
AdityaHadesta <126840205+AdityaHadesta@users.noreply.github.com>
2023-03-06 13:33:03 +0700

    Add files via upload

---
+++ AritmatikaDemo.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aditya_030223;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AritmatikaDemo 
+{
+    public static void main(String[] args)
+    {
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+        
+        System.out.println("Variable values...");
+        System.out.println("    i = "+ i);
+        System.out.println("    j = "+ j);
+        System.out.println("    x = "+ x);
+        System.out.println("    y = "+ y);
+        
+        
+        // Penambahan Angka
+        System.out.println("Adding...");
+        System.out.println("    i + j = "+ (i + j));
+        System.out.println("    x + y = "+ (x + y));
+        
+        //Pengurangan Angka
+        System.out.println("    i - j = "+ (i - j));
+        System.out.println("    x - y = "+ (x - y));
+        
+        // Perkalian Angka
+        System.out.println("    i * j = "+ (i * j));
+        System.out.println("    x * y = "+ (x * y));
+        
+        // Pembagian Angka
+        System.out.println("    i / j = "+ (i / j));
+        System.out.println("    x / y = "+ (x / y));
+        
+        // Menghitung hasil moduls dari pembagian
+        System.out.println("Computing the remainder...");
+        System.out.println("    i % j = "+ (i % j));
+        System.out.println("    x & y = "+ (x % y));
+        
+        // Tipe Penggabungan
+        System.out.println("Mixing tipes...");
+        System.out.println("    i + j = "+ (i + j));
+        System.out.println("    x * y = "+ (i * j));
+    }
+}
